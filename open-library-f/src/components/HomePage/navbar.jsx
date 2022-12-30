848495efa6ffca525a37000d297f0a9966150ce8
enas87 <eng.enas.87@gmail.com>
2022-12-30 07:49:50 +0200

    bootstrap

--- open-library-f/src/components/HomePage/navbar.jsx
+++ open-library-f/src/components/HomePage/navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
 const navbar = () => {
   return (
+    <>
     <div id='nav'>
-        <h1>Navbar here</h1>
+      <div className="container-fluid">
+		    <div className="container d-flex justify-content-between">
+			    <div><a href="#"><img src="../images/ia-logo.svg" width="160"/></a></div>
+			    <div><a href=""><img src="images/language-icon.svg" width="30"/></a></div>
+		    </div>
+      </div>
+	    <div class="nav_bar container my-4">
+        <ul class="d-flex">
+          <li><button><img src="images/openlibrary-logo-tighter.svg" width="189" height="47"/></button></li>
+          <li><button>My Books</button></li>
+          <li><button><span>Browse</span><span></span></button></li>
+          <li></li>
+          <li><button>Log In</button></li>
+          <li></li>
+			    <li><button></button></li>
+		    </ul>
+	    </div>
     </div>
+    </>
   )
 }
 
